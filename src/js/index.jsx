a4d362c7127cd8753632dc7688242c4b8f36ea05
BenoitArguel <benoit.arguel@gmail.com>
2016-04-21 17:08:20 +0200

    task: adding style

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,3 +1,20 @@
-import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+var BattleWidget = React.createClass({
+  render() {
+    return <div>
+      <div className="contentloader"> 
+        <div className="loader loader1"></div>
+      </div>
+      <div className="contentloader"> 
+        <div className="loader loader2"></div>
+      </div>
+    </div>;
+  }
+});
+
+ReactDOM.render(
+  <BattleWidget/>,
+  document.getElementById('container')
+);
