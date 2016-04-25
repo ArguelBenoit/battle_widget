7b4b691aa15f00ae11c844337d404b0d28f02197
Franck Ernewein <franck.ernewein@gmail.com>
2016-04-25 12:14:49 +0200

    task: split component in a separate file

--- src/js/BattleWidget.jsx
+++ src/js/BattleWidget.jsx
@@ -1,43 +1,49 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 
-var BattleWidget = React.createClass({
-  render() {
-    const levelFighter = [178, 138, 211];
-    const styleContainer = {
-      width: levelFighter.length * 100,
-      height: 200
-    };
-    const styleLoader1 = {
-      height: levelFighter[0],
-      marginTop: 200 - levelFighter[0]
-    };
-    const styleLoader2 = {
-      height: levelFighter[1],
-      marginTop: 200 - levelFighter[1]
-    };
-    const styleLoader3 = {
-      height: levelFighter[2],
-      marginTop: 200 - levelFighter[2]
-    };
-    const styleLoader4 = {
-      height: levelFighter[3],
-      marginTop: 200 - levelFighter[3]
-    };
-    const styleUnitLine = {
-      width: levelFighter.length * 100
-    };
-    return <div id="container" style={styleContainer}>
-      <div className="loader" style={styleLoader1}></div>
-      <div className="loader" style={styleLoader2}></div>
-      <div className="loader" style={styleLoader3}></div>
-      <div className="loader" style={styleLoader4}></div>
-      <hr className="unitline" style={styleUnitLine}/>
-      <hr className="unitline" style={styleUnitLine}/>
-      <hr className="unitline" style={styleUnitLine}/>
-      <hr className="unitline" style={styleUnitLine}/>
-      <hr className="unitline" style={styleUnitLine}/>
-    </div>;
-  }
-});
+function BattleWidget(props){
+  
+  //const levelFighter = props.levelFighter;
+  const {levelFighter} = props;
+
+  const styleContainer = {
+    width: levelFighter.length * 100,
+    height: 200
+  };
+  const styleLoader1 = {
+    height: levelFighter[0],
+    marginTop: 200 - levelFighter[0]
+  };
+  const styleLoader2 = {
+    height: levelFighter[1],
+    marginTop: 200 - levelFighter[1]
+  };
+  const styleLoader3 = {
+    height: levelFighter[2],
+    marginTop: 200 - levelFighter[2]
+  };
+  const styleLoader4 = {
+    height: levelFighter[3],
+    marginTop: 200 - levelFighter[3]
+  };
+  const styleUnitLine = {
+    width: levelFighter.length * 100
+  };
+  return <div id="container" style={styleContainer}>
+    <div className="loader" style={styleLoader1}></div>
+    <div className="loader" style={styleLoader2}></div>
+    <div className="loader" style={styleLoader3}></div>
+    <div className="loader" style={styleLoader4}></div>
+    <hr className="unitline" style={styleUnitLine}/>
+    <hr className="unitline" style={styleUnitLine}/>
+    <hr className="unitline" style={styleUnitLine}/>
+    <hr className="unitline" style={styleUnitLine}/>
+    <hr className="unitline" style={styleUnitLine}/>
+  </div>;
+}
+
+BattleWidget.propTypes = {
+  levelFighter: PropTypes.array
+};
+
 
 export default BattleWidget;
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BattleWidget from './BattleWidget.jsx';
 
+const data = [178, 138, 211];
+
 ReactDOM.render(
-  <BattleWidget />,
+  <BattleWidget levelFighter={data} />,
   document.getElementById('widgetbattle')
 );
