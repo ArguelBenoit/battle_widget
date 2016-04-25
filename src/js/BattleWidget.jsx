4998b89a6298c87f30748e7a159e20c40a4c43bf
Franck Ernewein <franck.ernewein@gmail.com>
2016-04-25 12:07:41 +0200

    task: split component in a separate file

---
+++ src/js/BattleWidget.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+var BattleWidget = React.createClass({
+  render() {
+    const levelFighter = [178, 138, 211];
+    const styleContainer = {
+      width: levelFighter.length * 100,
+      height: 200
+    };
+    const styleLoader1 = {
+      height: levelFighter[0],
+      marginTop: 200 - levelFighter[0]
+    };
+    const styleLoader2 = {
+      height: levelFighter[1],
+      marginTop: 200 - levelFighter[1]
+    };
+    const styleLoader3 = {
+      height: levelFighter[2],
+      marginTop: 200 - levelFighter[2]
+    };
+    const styleLoader4 = {
+      height: levelFighter[3],
+      marginTop: 200 - levelFighter[3]
+    };
+    const styleUnitLine = {
+      width: levelFighter.length * 100
+    };
+    return <div id="container" style={styleContainer}>
+      <div className="loader" style={styleLoader1}></div>
+      <div className="loader" style={styleLoader2}></div>
+      <div className="loader" style={styleLoader3}></div>
+      <div className="loader" style={styleLoader4}></div>
+      <hr className="unitline" style={styleUnitLine}/>
+      <hr className="unitline" style={styleUnitLine}/>
+      <hr className="unitline" style={styleUnitLine}/>
+      <hr className="unitline" style={styleUnitLine}/>
+      <hr className="unitline" style={styleUnitLine}/>
+    </div>;
+  }
+});
+
+export default BattleWidget;
