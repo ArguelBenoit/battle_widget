8c97c32605ec9ea7a337b97f67601e7d3cd8b15a
BenoitArguel <benoit.arguel@gmail.com>
2016-04-25 11:45:12 +0200

    task: adding unitLine

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,20 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// var a = [1, 2, 3];
+// var b = a.map(function(n) {
+//   console.log(n);
+//   return n * 2;
+// });
+// console.log(b);
+
+
 var BattleWidget = React.createClass({
   render() {
-    return <div>
-      <div className="contentloader"> 
-        <div className="loader loader1"></div>
-      </div>
-      <div className="contentloader"> 
-        <div className="loader loader2"></div>
-      </div>
+
+    const levelFighter = [178, 138, 211];
+
+    const styleContainer = {
+      width: levelFighter.length * 100,
+      height: 200
+    };
+    const styleLoader1 = {
+      height: levelFighter[0],
+      marginTop: 200 - levelFighter[0]
+    };
+    const styleLoader2 = {
+      height: levelFighter[1],
+      marginTop: 200 - levelFighter[1]
+    };
+    const styleLoader3 = {
+      height: levelFighter[2],
+      marginTop: 200 - levelFighter[2]
+    };
+    const styleUnitLine = {
+      width: levelFighter.length * 100
+    };
+    return <div id="container" style={styleContainer}>
+      <div className="loader loader1" style={styleLoader1}></div>
+      <div className="loader loader2" style={styleLoader2}></div>
+      <div className="loader loader2" style={styleLoader3}></div>
+      <hr className="unitline" style={styleUnitLine}/>
+      <hr className="unitline" style={styleUnitLine}/>
+      <hr className="unitline" style={styleUnitLine}/>
+      <hr className="unitline" style={styleUnitLine}/>
+      <hr className="unitline" style={styleUnitLine}/>
     </div>;
   }
 });
 
 ReactDOM.render(
   <BattleWidget/>,
-  document.getElementById('container')
+  document.getElementById('widgetbattle')
 );
